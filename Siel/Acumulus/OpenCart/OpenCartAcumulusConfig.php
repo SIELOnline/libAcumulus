0110ed52207de8b9fb6b8d1170fc883d97af935d
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2014-02-03 16:52:36 +0100

    New OpenCart release

--- Siel/Acumulus/OpenCart/OpenCartAcumulusConfig.php
+++ Siel/Acumulus/OpenCart/OpenCartAcumulusConfig.php
@@ -22,7 +22,7 @@ class OpenCartAcumulusConfig extends BaseConfig {
    *
    * @var string
    */
-  public static $module_version = '3.2.0';
+  public static $module_version = '3.2.1';
 
 
   /** @var \ModelSettingSetting */

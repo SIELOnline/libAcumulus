8b10bb79959948e43fa7a7d6bbb26581583b845c
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2014-06-13 11:57:02 +0200

    3.3.4: was not released, so previous commit can be part of 3.3.4.

--- Siel/Acumulus/Common/BaseConfig.php
+++ Siel/Acumulus/Common/BaseConfig.php
@@ -14,7 +14,7 @@ abstract class BaseConfig implements ConfigInterface{
    *
    * @var string
    */
-  public static $library_version = '3.3.5-beta1';
+  public static $library_version = '3.3.4';
 
   /** @var bool */
   protected $isLoaded;

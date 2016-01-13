f2dbbf3db42cad3e09e24c7812fad9b0b1777587
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-01-13 20:42:44 +0100

    4.2.0-beta7: Make use of WC logging instead of very basic (=absent) WP logging.

--- Siel/Acumulus/WooCommerce/Helpers/Log.php
+++ Siel/Acumulus/WooCommerce/Helpers/Log.php
@@ -2,6 +2,7 @@
 namespace Siel\Acumulus\WooCommerce\Helpers;
 
 use Siel\Acumulus\Helpers\Log as BaseLog;
+use WC_Logger;
 
 /**
  * Extends the base log class to log any library logging to the WP log.
@@ -14,9 +15,9 @@ class Log extends BaseLog {
    * This override checks for WP_DEBUG first.
    */
   protected function write($message, $severity) {
-    if (WP_DEBUG) {
-      parent::write($message, $severity);
-    }
+    $logger = new WC_Logger();
+    $message = sprintf('%s - %s', $this->getSeverityString($severity), $message);
+    $logger->add('acumulus', $message);
   }
 
 }

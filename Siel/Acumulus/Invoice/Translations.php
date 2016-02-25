d74c6a6bc192cd42c71c3988b959b6b916dc243b
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-02-25 16:15:06 +0100

    Waarschuwingsteksten

--- Siel/Acumulus/Invoice/Translations.php
+++ Siel/Acumulus/Invoice/Translations.php
@@ -32,10 +32,10 @@ class Translations extends TranslationCollection {
     'fee_adjustment' => 'Kosten (soort onbekend)',
     'discount_adjustment' => 'Handmatige korting',
 
-    'message_warning_no_vat' => 'De factuur bevat geen BTW en er kan niet bepaald worden welk BTW-type van toepassing is. De factuur is als concept opgeslagen. Corrigeer de factuur in Acumulus en controleer uw BTW instellingen.',
-    'message_warning_no_vattype' => 'Door een fout in uw instellingen of BTW tarieven, kan het BTW-type van de factuur niet bepaald worden. De factuur is als concept opgeslagen. Corrigeer of completeer de factuur in Acumulus.',
-    'message_warning_multiple_vattype_must_split' => 'De factuur heeft meerdere BTW-types. De factuur is als concept opgeslagen. Splits de factuur in Acumulus en verdeel de regels over beide facturen gebaseerd op het BTW type waar de regel onder valt.',
-    'message_warning_multiple_vattype_may_split' => 'De factuur kan meerdere BTW-types hebben. De factuur is als concept opgeslagen. Controleer het BTW type van de factuur in Acumulus en corrigeer deze indien nodig, of splits de factuur en verdeel de regels over beide facturen gebaseerd op het BTW type waar de regel onder valt.',
+    'message_warning_no_vat' => 'De factuur bevat geen BTW en er kan niet bepaald worden welk factuurtype (https://wiki.acumulus.nl/index.php?page=127) van toepassing is. Daarom is de factuur als concept opgeslagen. Corrigeer de factuur in Acumulus en controleer uw BTW instellingen.',
+    'message_warning_no_vattype' => 'Door een fout in uw instellingen of BTW tarieven, kan het factuurtype (https://wiki.acumulus.nl/index.php?page=127) niet bepaald worden. Daarom is de factuur is als concept opgeslagen. Corrigeer of completeer de factuur in Acumulus.',
+    'message_warning_multiple_vattype_must_split' => 'De factuur heeft meerdere factuurtypes (https://wiki.acumulus.nl/index.php?page=127). Daarom is de factuur als concept opgeslagen. Splits de factuur in Acumulus en verdeel de regels over beide facturen gebaseerd op het BTW type waar de regel onder valt.',
+    'message_warning_multiple_vattype_may_split' => 'De factuur kan meerdere factuurtypes hebben (https://wiki.acumulus.nl/index.php?page=127). Daarom is de factuur als concept opgeslagen. Controleer het BTW type van de factuur in Acumulus en corrigeer deze indien nodig, of splits de factuur en verdeel de regels over beide facturen gebaseerd op het BTW type waar de regel onder valt.',
   );
 
   protected $en = array(
@@ -61,10 +61,10 @@ class Translations extends TranslationCollection {
     'fee_adjustment' => 'Fee (unknown type)',
     'discount_adjustment' => 'Manual discount',
 
-    'message_warning_no_vat' => 'The invoice has no VAT and it is not possible to determine the VAT type. The invoice has been saved as concept. Correct the invoice in acumulus and check your VAT settings.',
-    'message_warning_no_vattype' => 'Due to an error in your settings, we cannot determine the VAT type for the invoice. The invoice has been saved as concept. Correct or complete the invoice in Acumulus.',
-    'message_warning_multiple_vattype_must_split' => 'The invoice has multiple VAT types. The invoice has been saved as concept. Split the invoice in Acumulus and divide the invoice lines over both invoices based on the VAT type the line belongs to.',
-    'message_warning_multiple_vattype_may_split' => 'The invoice can have multiple VAT types. The invoice has been saved as concept. Check the VAT type of the invoice in Acumulus and correct if necessary, or split the invoice and divide the invoice lines over both invoices based on the VAT type the line belongs to.',
+    'message_warning_no_vat' => 'The invoice has no VAT and it is not possible to determine the invoice type (https://wiki.acumulus.nl/index.php?page=127). The invoice has been saved as concept. Correct the invoice in acumulus and check your VAT settings.',
+    'message_warning_no_vattype' => 'Due to an error in your settings, we cannot determine the invoice type for the invoice (https://wiki.acumulus.nl/index.php?page=127). The invoice has been saved as concept. Correct or complete the invoice in Acumulus.',
+    'message_warning_multiple_vattype_must_split' => 'The invoice has multiple invoice types (https://wiki.acumulus.nl/index.php?page=127). The invoice has been saved as concept. Split the invoice in Acumulus and divide the invoice lines over both invoices based on the VAT type the line belongs to.',
+    'message_warning_multiple_vattype_may_split' => 'The invoice can have multiple invoice types (https://wiki.acumulus.nl/index.php?page=127). The invoice has been saved as concept. Check the VAT type of the invoice in Acumulus and correct if necessary, or split the invoice and divide the invoice lines over both invoices based on the VAT type the line belongs to.',
   );
 
 }

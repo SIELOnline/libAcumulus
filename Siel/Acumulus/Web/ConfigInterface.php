12efe0b5be3ac2ad6c0b30c20d808522c6eeabbb
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-06-01 11:24:03 +0200

    4.5.0-beta2

--- Siel/Acumulus/Web/ConfigInterface.php
+++ Siel/Acumulus/Web/ConfigInterface.php
@@ -10,7 +10,7 @@ namespace Siel\Acumulus\Web;
  */
 interface ConfigInterface
 {
-    const libraryVersion = '4.5.0-beta1';
+    const libraryVersion = '4.5.0-beta2';
 
     // Web service configuration related constants.
     const Status_NotSent = -1;

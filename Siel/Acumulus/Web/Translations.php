08ae96fae2a55b8cfa844b3be258942341cd7029
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2017-07-11 21:06:09 +0200

    Prevent 2 consecutive points ('.')

--- Siel/Acumulus/Web/Translations.php
+++ Siel/Acumulus/Web/Translations.php
@@ -15,12 +15,12 @@ class Translations extends TranslationCollection
         'message_warning' => 'Waarschuwing',
         'message_sent' => 'Verzonden bericht',
         'message_received' => 'Ontvangen bericht',
-        'message_response_success' => 'Succes, zonder waarschuwingen.',
-        'message_response_errors' => 'Mislukt, fouten gevonden.',
-        'message_response_warnings' => 'Succes, met waarschuwingen.',
-        'message_response_exception' => 'Fout, neem contact op met Acumulus.',
-        'message_response_unknown' => 'Onbekende status code %s.',
-        'message_response_not_set' => 'Status code nog niet gezet.',
+        'message_response_success' => 'Succes',
+        'message_response_errors' => 'Mislukt, fouten gevonden',
+        'message_response_warnings' => 'Succes, met waarschuwingen',
+        'message_response_exception' => 'Fout, neem contact op met Acumulus',
+        'message_response_unknown' => 'Onbekende status code %s',
+        'message_response_not_set' => 'Status code nog niet gezet',
     );
 
     protected $en = array(
@@ -29,11 +29,11 @@ class Translations extends TranslationCollection
         'message_warning' => 'Warning',
         'message_sent' => 'Message sent',
         'message_received' => 'Message received',
-        'message_response_success' => 'Success, without warnings.',
-        'message_response_errors' => 'Failed, errors found.',
-        'message_response_warnings' => 'Success, with warnings.',
-        'message_response_exception' => 'Exception, please contact Acumulus technical support.',
-        'message_response_unknown' => 'Unknown status code %s.',
-        'message_response_not_set' => 'Status code not yet set.',
+        'message_response_success' => 'Success',
+        'message_response_errors' => 'Failed, errors found',
+        'message_response_warnings' => 'Success, with warnings',
+        'message_response_exception' => 'Exception, please contact Acumulus technical support',
+        'message_response_unknown' => 'Unknown status code %s',
+        'message_response_not_set' => 'Status code not yet set',
     );
 }

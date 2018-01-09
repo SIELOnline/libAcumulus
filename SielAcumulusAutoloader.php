0e6d0ac1a7fd3452d86f4493d67872fd3dd06e57
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2018-01-09 14:22:01 +0100

    Autoloader back in the library.

---
+++ SielAcumulusAutoloader.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Registers an autoloader for the Siel\Acumulus namespace.
+ *
+ * As not all web shops support auto-loading based on namespaces or have
+ * other glitches, eg. expecting lower cased file names, we define our own
+ * autoloader. If the module cannot use the autoloader of the web shop, this
+ * file/class should be loaded during bootstrapping of the module and the
+ * register method should called.
+ *
+ * Thanks to https://gist.github.com/mageekguy/8300961
+ */
+class SielAcumulusAutoloader
+{
+    public static function register()
+    {
+        $dir = __DIR__ . '/src/';
+        $ourNamespace = 'Siel\\Acumulus\\';
+        $ourNamespaceLen = strlen($ourNamespace);
+        $autoloadFunction = function ($class) use ($ourNamespace, $ourNamespaceLen, $dir) {
+            if (strncmp($class, $ourNamespace, $ourNamespaceLen) === 0) {
+                $fileName = $dir . str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $ourNamespaceLen)) . '.php';
+                if (is_readable($fileName)) {
+                    /** @noinspection PhpIncludeInspection */
+                    include($fileName);
+                }
+            }
+        };
+        // Prepend this autoloader: it will not throw, nor warn, while the shop
+        // specific autoloader might do so.
+        spl_autoload_register($autoloadFunction, true, true);
+    }
+}

364a327d1b0abbba39c1345c80084d40fb057532
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2018-02-28 18:23:57 +0100

    oops: internal version not updated

--- src/PluginConfig.php
+++ src/PluginConfig.php
@@ -8,7 +8,7 @@ namespace Siel\Acumulus;
  */
 interface PluginConfig
 {
-    const Version = '5.1.2';
+    const Version = '5.2.0';
 
     const Send_SendAndMailOnError = 1;
     const Send_SendAndMail = 2;

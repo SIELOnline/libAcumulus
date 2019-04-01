5f6a8032c89d13ca3918bfaf24d9d6ba6d54c3a8
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2019-04-01 16:06:39 +0200

    5.6.1

--- src/PluginConfig.php
+++ src/PluginConfig.php
@@ -17,7 +17,7 @@ interface PluginConfig
      *
      * @var string
      */
-    const Version = '5.6.0';
+    const Version = '5.6.1';
 
     /**
      * Format to use with date() and DateTime formatting methods for SQL

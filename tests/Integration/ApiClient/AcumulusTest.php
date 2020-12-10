cbaf3bda24cab473569e5d97172c4e079ead42ae
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2020-12-10 15:42:28 +0100

    Add tests for the getVatInfo() call to prepare using it for the isEu() method.

--- tests/Integration/ApiClient/AcumulusTest.php
+++ tests/Integration/ApiClient/AcumulusTest.php
@@ -28,8 +28,8 @@ class AcumulusTest extends TestCase
     public function responseKeysProvider()
     {
         return [
-            'About' => ['getAbout', [], false, ['about', 'role', 'roleid']],
-            'MyAcumulus' => ['getMyAcumulus', [], false, ['mycontractcode','mycompanyname','mycontactperson','myaddress','mypostalcode','mycity','mytelephone','myemail','myiban','mysepamandatenr','mycontractenddate']],
+            'About' => ['getAbout', [], false, ['about', 'role', 'roleapi', 'roleid', 'rolenl']],
+            'MyAcumulus' => ['getMyAcumulus', [], false, ['myaddress','mycity','mycompanyname','mycontactperson','mycontractcode','mycontractenddate','mydebt','myemail','myemailstatusid','myemailstatusreferenceid','myentries','myentriesleft','myiban','mymaxentries','mypostalcode','mysalutation','mysepamandatenr','mystatusid','mytelephone','myvatnumber']],
             'Accounts' => ['getPicklistAccounts', [], true, ['accountid', 'accountnumber', 'accountdescription','accounttypeid']],
             'ContactTypes' => ['getPicklistContactTypes', [], true, ['contacttypeid', 'contacttypename','contacttypenamenl']],
             'CostCenters' => ['getPicklistCostCenters', [], true, ['costcenterid','costcentername']],
@@ -60,6 +60,39 @@ class AcumulusTest extends TestCase
         $this->assertNotEmpty($singleResponse);
         $this->assertEqualsCanonicalizing($expectedKeys, array_keys($singleResponse));
     }
+
+    public function vatInfoProvider()
+    {
+        return [
+            'nl' => [['nl', '2015-01-01'], [['vattype' => 'reduced','vatrate' => '6.0000'],['vattype' => 'normal','vatrate' => '21.0000']]],
+            'nl-no-date' => [['nl'], [['vattype' => 'reduced','vatrate' => '9.0000'],['vattype' => 'normal','vatrate' => '21.0000']]],
+            'eu' => [['be', '2015-12-01'], [['vattype' => 'reduced','vatrate' => '6.0000'],['vattype' => 'reduced','vatrate' => '12.0000'],['vattype' => 'normal','vatrate' => '21.0000'],['vattype' => 'parked','vatrate' => '12.0000'],['vattype' => 'reduced','vatrate' => '0.0000']]],
+            'eu-no-date' => [['be'], [['vattype' => 'reduced','vatrate' => '6.0000'],['vattype' => 'reduced','vatrate' => '12.0000'],['vattype' => 'normal','vatrate' => '21.0000'],['vattype' => 'parked','vatrate' => '12.0000'],['vattype' => 'reduced','vatrate' => '0.0000']]],
+            'eu-wrong-date' => [['be', '2014-12-01'], []],
+            'non-eu' => [['af', '2020-12-01'], ['vattype','vatrate']],
+        ];
+    }
+
+    /**
+     * @dataProvider vatInfoProvider
+     */
+    public function testGetVatInfo(array $args, array $expected)
+    {
+        $result = $this->acumulusClient->getVatInfo(... $args);
+        $this->assertSame(Severity::Success, $result->getStatus());
+
+        $vatrate  = array_column($expected, 'vatrate');
+        $vattype = array_column($expected, 'vattype');
+        array_multisort($vatrate, SORT_DESC, $vattype, SORT_ASC, $expected);
+
+        $actual = $result->getResponse();
+        $vatrate  = array_column($actual, 'vatrate');
+        $vattype = array_column($actual, 'vattype');
+        array_multisort($vatrate, SORT_DESC, $vattype, SORT_ASC, $actual);
+
+        $this->assertEquals($expected, $actual);
+    }
+
 
 /*
     public function testInvoiceAdd()

53aeefe08589d59b0d4dac4203b65b70ef176bdc
Erwin Derksen <erwin@burorader.com>
2021-11-14 20:10:59 +0100

    WC: de method WC_Logger::add() is deprecated. Onze log helper is hier op aangepast Zichtbaar gevolg: de datum wordt nu in de ISO standaard afgedrukt.

--- src/WooCommerce/Helpers/Log.php
+++ src/WooCommerce/Helpers/Log.php
@@ -2,6 +2,7 @@
 namespace Siel\Acumulus\WooCommerce\Helpers;
 
 use Siel\Acumulus\Helpers\Log as BaseLog;
+use Siel\Acumulus\Helpers\Severity;
 use WC_Logger;
 
 /**
@@ -16,8 +17,43 @@ class Log extends BaseLog
      */
     protected function write($message, $severity)
     {
-        $logger = new WC_Logger();
-        $message = sprintf('%s - %s', $this->getSeverityString($severity), $message);
-        $logger->add('acumulus', $message);
+        (new WC_Logger())->log($this->getWooCommerceSeverity($severity), $message, ['source' => 'acumulus']);
+    }
+
+    /**
+     * Returns the WooCommerce equivalent of the severity.
+     *
+     * @param int $severity
+     *   One of the Severity::... constants.
+     *
+     * @return string
+     *   The WooCommerce equivalent of the severity, @see WC_Logger::Log() for
+     *   a list of severities:
+     *   - 'emergency': System is unusable.
+     *   - 'alert': Action must be taken immediately.
+     *   - 'critical': Critical conditions.
+     *   - 'error': Error conditions.
+     *   - 'warning': Warning conditions.
+     *   - 'notice': Normal but significant condition.
+     *   - 'info': Informational messages.
+     *   - 'debug': Debug-level messages.
+     */
+    protected function getWooCommerceSeverity($severity)
+    {
+        switch ($severity) {
+            case Severity::Exception:
+                return 'critical';
+            case Severity::Error:
+                return 'error';
+            case Severity::Warning:
+                return 'warning';
+            case Severity::Notice:
+                return 'notice';
+            case Severity::Info:
+                return 'info';
+            case Severity::Log:
+            default:
+                return 'debug';
+        }
     }
 }

046bf921d837c330d1ba034ab66d1ae221177b7a
Erwin Derksen <erwin@burorader.com>
2022-06-10 21:19:37 +0200

    Added translations for the "Activate pro-support" form.

--- src/Shop/ActivateSupportFormTranslations.php
+++ src/Shop/ActivateSupportFormTranslations.php
@@ -12,7 +12,7 @@ class ActivateSupportFormTranslations extends TranslationCollection
         'activate_form_title' => 'Acumulus | Activeer Pro-support',
         'activate_form_header' => 'Activeer Pro-support voor uw Acumulus webshopkoppeling',
         'activate_form_link_text' => 'Activeer Acumulus pro-support',
-        'message_form_activate_success' => 'Ondersteuning voor uw webshopkoppeling is met success geactiveerd. Zie het "Over" blok verderop op deze pagina.',
+        'message_form_activate_success' => 'Pro-support voor uw webshopkoppeling is met success geactiveerd. Zie het "Over" blok verderop op deze pagina.',
 
         'button_submit_activate'=> 'Activeer',
         'button_cancel' => 'Annuleren',
@@ -28,25 +28,21 @@ class ActivateSupportFormTranslations extends TranslationCollection
    ];
 
     protected $en = [
-        'activate_form_title' => 'Acumulus | Activeer Pro-support',
-        'activate_form_header' => 'Activeer Pro-support voor uw Acumulus webshop koppeling',
-        'activate_form_link_text' => 'Activeer Acumulus pro-support',
+        'activate_form_title' => 'Acumulus | Activate Pro-support',
+        'activate_form_header' => 'Activate Pro-support for your Acumulus webshop connector',
+        'activate_form_link_text' => 'Activate Acumulus pro-support',
+        'message_form_activate_success' => 'Pro-support for your webshop connector has been successfully activated. See the "About" block further down this page.',
 
-        'button_submit_activate'=> 'Activeer',
-        'button_cancel' => 'Annuleren',
+        'button_submit_activate'=> 'Activate',
+        'button_cancel' => 'Cancel',
 
-        'activateFieldsHeader' => 'Activeer uw aangekochte pro-support voor deze website',
+        'activateFieldsHeader' => 'Activate the pro-support you bought for this webshop %1$s',
         'field_activate_token' => 'Token',
-        'desc_activate_token' => 'Vul hier het token in dat u ontvangen heeft nadat u online bij Siel de pro-support voor deze webshop aankocht.',
-        'field_activate_webshop' => 'Webshop',
-        'desc_activate_webshop' => 'Aankoop van 1 jaar pro-support is in principe voor 1 webshop. Het domeinnaam van deze webshop (zonder een evt. "www.") wordt meegestuurd bij de activatie.',
+        'desc_activate_token' => 'Fill in the token that you received after buying pro-support for this webshop %1$s.',
+        'field_activate_website' => 'Webshop',
+        'desc_activate_website' => 'The purchase of 1 year of pro-support is normally for 1 webshop. The domain name of the webshop (without any "www.") will be sent along with the activation.',
 
-        'supportInfoHeader' => 'Toelichting op Pro-support',
-        'support_info' => <<<LONGSTRING
-<p>TODO1: Leg uit wat dit formulier doet.</p>
-<p>TODO2: Leg uit wat pro-support is: verwijs naar https://www.siel.nl/acumulus/koppelingen/support/.</p>
-<p>TODO3: neem link naar webshop specifieke pro-support aankoop in de siel winkel op.</p>
-LONGSTRING
-        ,
+        'message_validate_invalid_token' => 'The token is invalid.',
+        'message_validate_activate_hostname_changed' => 'The domain name of your website differs from when you filled in this form.',
     ];
 }

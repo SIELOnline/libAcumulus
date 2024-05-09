38e1afbcfac9fc575e79796d12ae1c7ec76d83d2
Erwin Derksen <erwin@burorader.com>
2024-05-09 20:29:30 +0200

    docu

--- src/PrestaShop/Collectors/InvoiceCollector.php
+++ src/PrestaShop/Collectors/InvoiceCollector.php
@@ -16,6 +16,8 @@ class InvoiceCollector extends BaseInvoiceCollector
     protected function collectLogicFields(AcumulusObject $acumulusObject): void
     {
         parent::collectLogicFields($acumulusObject);
+        // In PrestaShop you can enter a price with or without vat, the other being
+        // automatically updated. So we can not know how prices where entered.
         $acumulusObject->metadataSet(Meta::PricesIncludeVat, null);
     }
 }

d0491af1c74d31239a334ec34da7fbf2ae211f99
Erwin Derksen <erwin@burorader.com>
2024-09-12 10:33:00 +0200

    Cleaned up WooCommerce/Invoice/Creator after moving item line code to collectors

--- src/WooCommerce/Invoice/Creator.php
+++ src/WooCommerce/Invoice/Creator.php
@@ -26,8 +26,6 @@ use Siel\Acumulus\Config\Config;
 use Siel\Acumulus\Tag;
 use WC_Coupon;
 use WC_Order_Item_Fee;
-use WC_Order_Item_Product;
-use WC_Product;
 use WC_Tax;
 
 use function count;
@@ -44,11 +42,6 @@ use function strlen;
 class Creator extends BaseCreator
 {
     /**
-     * Whether the order has (non-empty) item lines.
-     */
-    protected bool $hasItemLines;
-
-    /**
      * Product price precision in WC3: one of the prices is entered by the
      * administrator and may be assumed exact. The computed one is based on the
      * subtraction/addition of 2 amounts, so has a precision that may be twice
@@ -60,157 +53,6 @@ class Creator extends BaseCreator
      * possible.
      */
     protected float $precision = 0.01;
-
-    protected function getItemLines(): array
-    {
-        $result = [];
-        /** @var WC_Order_Item_Product[] $items */
-        $items = $this->invoiceSource->getSource()->get_items(apply_filters('woocommerce_admin_order_item_types', 'line_item'));
-        foreach ($items as $item) {
-            $product = $item->get_product();
-            $line = $this->getItemLine($item, $product);
-            if ($line) {
-                $result[] = $line;
-            }
-        }
-
-        $this->hasItemLines = count($result) > 0;
-        return $result;
-    }
-
-    /**
-     * Returns 1 item line.
-     * Some processing notes:
-     * - Though recently I did not see it anymore, in the past I have seen
-     *   refunds where articles that were not returned were still listed but
-     *   with qty = 0 (and line total = 0).
-     * - It turns out that you can do partial refunds by entering a broken
-     *   number in the quantity field when defining a refund on the edit order
-     *   page. However, the quantity stored is still rounded towards zero and
-     *   thus may result in qty = 0 but line total != 0 or just item price not
-     *   being equal to line total divided by the qty.
-     *
-     * @param WC_Order_Item_Product $item
-     *   An object representing an order item line.
-     * @param WC_Product|bool|null $product
-     *   The product that was sold on this line, may also be a bool according to
-     *   the WC3 php documentation. I guess it will be false if the product has
-     *   been deleted since.
-     *
-     * @return array
-     *   May be empty if the line should not be sent (e.g. qty = 0 on a refund).
-     */
-    protected function getItemLine(WC_Order_Item_Product $item, $product): array
-    {
-        $result = [];
-
-        // Return if this "really" is an empty line, not when this is a line
-        // with free products or an amount but without a quantity.
-        $quantity = (float) $item->get_quantity();
-        $total = (float) $item->get_total();
-        if (Number::isZero($quantity) && Number::isZero($total)) {
-            return $result;
-        }
-
-        // Support for some often used plugins that extend WooCommerce,
-        // especially in the area of products (bundles, bookings, ...).
-        /** @var \Siel\Acumulus\WooCommerce\Invoice\CreatorPluginSupport $creatorPluginSupport */
-        $creatorPluginSupport = $this->getContainer()->getInstance('CreatorPluginSupport', 'Invoice');
-        $creatorPluginSupport->getItemLineBefore($this, $item, $product);
-
-        // $product can be null if the product has been deleted.
-        if ($product instanceof WC_Product) {
-            $this->addPropertySource('product', $product);
-        }
-        $this->addPropertySource('item', $item);
-
-        $result[Meta::Id] = $item->get_id();
-
-        // Add quantity: quantity is negative on refunds, the unit price will be
-        // positive.
-        // Correct if 0.
-        if (Number::isZero($quantity)) {
-            $quantity = $this->invoiceSource->getSign();
-        }
-        $commonTags = [Tag::Quantity => $quantity];
-        $result += $commonTags;
-
-        // Add price info. get_total() and get_taxes() return line totals after
-        // discount. get_taxes() returns non-rounded tax amounts per tax class
-        // id, whereas get_total_tax() returns either a rounded or non-rounded
-        // amount, depending on the 'woocommerce_tax_round_at_subtotal' setting.
-        $productPriceEx = $total / $quantity;
-        $taxes = $item->get_taxes()['total'];
-        $productVat = array_sum($taxes) / $quantity;
-        $productPriceInc = $productPriceEx + $productVat;
-
-        // Get precision info.
-        if ($this->productPricesIncludeTax()) {
-            // In the past I have seen WooCommerce store rounded vat amounts
-            // together with a not rounded ex price. If that is the case, the
-            // precision of the - calculated - inc price is not best, and we
-            // should not recalculate the price ex when we have obtained a
-            // corrected vat rate as that will worsen the precision of the price
-            // ex.
-            $precisionEx = $this->precision;
-            $reason = $this->isPriceIncRealistic($productPriceInc, $taxes, $product);
-            if ($reason !== '') {
-                $this->addWarning($result, "Price inc is realistic: $reason", Meta::Info);
-                $precisionInc = 0.001;
-                $recalculatePrice = Tag::UnitPrice;
-            } else {
-                $precisionInc = 0.02;
-                $recalculatePrice = Meta::UnitPriceInc;
-            }
-        } else {
-            $precisionEx = 0.001;
-            $precisionInc = $this->precision;
-            $recalculatePrice = Meta::UnitPriceInc;
-        }
-        // Note: this assumes that line calculations are done in a very precise
-        // way (in other words: total_tax has not a precision of
-        // base_precision * quantity) ...
-        $precisionVat = max(abs($this->precision / $quantity), 0.001);
-
-        // Check for cost price and margin scheme.
-        if (!empty($line['costPrice']) && $this->allowMarginScheme()) {
-            // Margin scheme:
-            // - Do not put VAT on invoice: send price incl VAT as 'unitprice'.
-            // - But still send the VAT rate to Acumulus.
-            $result += [
-                Tag::UnitPrice => $productPriceInc,
-            ];
-            $precisionEx = $precisionInc;
-        } else {
-            $result += [
-                Tag::UnitPrice => $productPriceEx,
-                Meta::UnitPriceInc => $productPriceInc,
-                Meta::PrecisionUnitPriceInc => $precisionInc,
-                Meta::RecalculatePrice => $recalculatePrice,
-            ];
-        }
-
-        // Add tax info.
-        $result += self::getVatRangeTags($productVat, $productPriceEx, $precisionVat, $precisionEx);
-        if ($product instanceof WC_Product) {
-            // get_tax_status() returns 'taxable', 'shipping', or 'none'.
-            $taxClass = $product->get_tax_status() === 'taxable' ? $product->get_tax_class() : null;
-            $result += $this->getVatRateLookupMetadataByTaxClass($taxClass);
-        }
-
-        // Add variants/options.
-        $commonTags[Meta::VatRateSource] = VatRateSource::Parent;
-        if ($product instanceof WC_Product && $item->get_variation_id()) {
-            $result[Meta::ChildrenLines] = $this->getVariantLines($item, $product, $commonTags);
-        }
-
-        $this->removePropertySource('product');
-        $this->removePropertySource('item');
-
-        $creatorPluginSupport->getItemLineAfter($this, $item, $product);
-
-        return $result;
-    }
 
     /**
      * Looks up and returns vat rate metadata for product lines.
@@ -267,94 +109,6 @@ class Creator extends BaseCreator
                 $result[Meta::VatRateLookupLabel][] = $taxRate['label'];
             }
         }
-        return $result;
-    }
-
-    /**
-     * Returns an array of lines that describes this variant.
-     * This method supports the default WooCommerce variant functionality.
-     *
-     * @param \WC_Order_Item_Product $item
-     * @param \WC_Product $product
-     * @param array $commonTags
-     *   An array of tags from the parent product to add to the child lines.
-     *
-     * @return array[]
-     *   An array of lines that describes this variant.
-     * @todo: Can $item->get_formatted_meta_data(''); be used to get this info?
-     */
-    protected function getVariantLines(WC_Order_Item_Product $item, WC_Product $product, array $commonTags): array
-    {
-        $result = [];
-
-        /**
-         * An array of objects with properties id, key, and value.
-         *
-         * @var object[] $metadata
-         */
-        $metadata = $item->get_meta_data();
-        if (count($metadata) > 0) {
-            // Define hidden core fields: check this when new versions from WC
-            // are released with the list in e.g.
-            // wp-content\plugins\woocommerce\includes\admin\meta-boxes\views\html-order-item-meta.php
-            $hiddenOrderItemMeta = apply_filters('woocommerce_hidden_order_itemmeta', [
-                    '_qty',
-                    '_tax_class',
-                    '_product_id',
-                    '_variation_id',
-                    '_line_subtotal',
-                    '_line_subtotal_tax',
-                    '_line_total',
-                    '_line_tax',
-                    'method_id',
-                    'cost',
-                    '_reduced_stock',
-                    '_restock_refunded_items',
-                ]
-            );
-            foreach ($metadata as $meta) {
-                // Skip hidden fields:
-                // - arrays
-                // - serialized data (which are also hidden fields)
-                // - tm extra product options plugin metadata which should be
-                //   removed by that plugin via the
-                //  'woocommerce_hidden_order_itemmeta' filter, but they don't.
-                // - all metadata keys starting with an underscore (_). This is
-                //   the convention for post metadata, but it is unclear if this
-                //   is also the case for woocommerce order item metadata, see
-                //   their own list versus the documentation on
-                //   https://developer.wordpress.org/plugins/metadata/managing-post-metadata/#hidden-custom-fields
-                if (in_array($meta->key, $hiddenOrderItemMeta, true)
-                    || is_array($meta->value)
-                    || is_serialized($meta->value)
-                    || substr($meta->key, 0, strlen('_')) === '_'
-                ) {
-                    continue;
-                }
-
-                // Get attribute data.
-                if (taxonomy_exists(wc_sanitize_taxonomy_name($meta->key))) {
-                    $term = get_term_by('slug', $meta->value, wc_sanitize_taxonomy_name($meta->key));
-                    $variantLabel = wc_attribute_label(wc_sanitize_taxonomy_name($meta->key));
-                    $variantValue = $term->name ?? $meta->value;
-                } else {
-                    $variantLabel = apply_filters(
-                        'woocommerce_attribute_label',
-                        wc_attribute_label($meta->key, $product),
-                        $meta->key,
-                        $product
-                    );
-                    $variantValue = $meta->value;
-                }
-
-                // @todo: Why a rawurldecode() here, is that a "filter" to apply?
-                $result[] = [
-                        Tag::Product => $variantLabel . ': ' . rawurldecode($variantValue),
-                        Tag::UnitPrice => 0,
-                    ] + $commonTags;
-            }
-        }
-
         return $result;
     }
 
@@ -656,49 +410,5 @@ class Creator extends BaseCreator
     {
         /** @noinspection PhpUndefinedFunctionInspection   false positive */
         return wc_prices_include_tax();
-    }
-
-    /**
-     * Returns whether the price inc can be considered realistic.
-     * Precondition: product prices as entered by the shop manager include tax
-     *  and thus can be considered to be expressed in cents.
-     * If a price inc is not considered realistic, we should not recalculate the
-     * product price ex based on the product price inc after we have obtained a
-     * corrected vat rate.
-     *
-     * @param float $productPriceInc
-     *   The product price including vat found on an item line, this includes
-     *   any discount.
-     * @param float[] $taxes
-     *   May be passed as strings.
-     * @param \WC_Product|bool|null $product
-     *   The product that has the given price inc and taxes.
-     *
-     * @return string
-     *   true if the price inc can be considered realistic, false otherwise.
-     */
-    protected function isPriceIncRealistic(float $productPriceInc, array $taxes, $product): string
-    {
-        $reason = '';
-        // Given the precondition that product prices as entered include vat, we
-        // consider a price in cents realistic.
-        if ((Number::isRounded($productPriceInc, 2))) {
-            $reason = "price inc is rounded: $productPriceInc";
-        }
-        // If the price equals the actual product price, we consider it
-        // realistic. Note that there may be valid reasons that the price differs
-        // from the actual price, e.g. a price change since the order was placed,
-        // or a discount that has been applied to the item line.
-        if ($product instanceof WC_Product) {
-            $productPriceOrg = $product->get_price();
-            if (Number::floatsAreEqual($productPriceInc, $productPriceOrg, 0.000051)) {
-                $reason = "item price inc ($productPriceInc) = product price inc ($productPriceOrg)";
-            }
-        }
-        if (!Number::areRounded($taxes, 2)) {
-            $reason = sprintf('not all taxes are rounded => taxes are realistic (%s)',
-                str_replace('"', "'", json_encode($taxes, Meta::JsonFlags)));
-        }
-        return $reason;
     }
 }

bc18258c405d0174a0b9ea6ab906f0ebcee5c2f3
Erwin Derksen <erwin@burorader.com>
2026-07-08 11:59:46 +0200

    First tests: get environment

--- src/Whmcs/Config/Environment.php
+++ src/Whmcs/Config/Environment.php
@@ -6,7 +6,9 @@ namespace Siel\Acumulus\Whmcs\Config;
 
 use Siel\Acumulus\Config\Environment as EnvironmentBase;
 use Siel\Acumulus\Meta;
+use Siel\Whmcs\Acumulus\Acumulus;
 use WHMCS\Application;
+use WHMCS\Database\Capsule;
 
 use function dirname;
 
@@ -17,9 +19,16 @@ class Environment extends EnvironmentBase
 {
     protected function setShopEnvironment(): void
     {
-        $addonFolder = dirname(__FILE__, 7); // @todo
-        $composer = json_decode(file_get_contents($addonFolder . '/composer.json'), true, flags: Meta::JsonFlags);
-        $this->data['moduleVersion'] = $composer->version ?? 'unknown';
-        $this->data['shopVersion'] = Application::getVersion(); // @todo
+        $this->data['moduleVersion'] = acumulus_get()->getVersion();
+        $results = localAPI('WhmcsDetails', []);
+        $this->data['shopVersion'] = $results['whmcs']['version'] ?? 'unknown';
+    }
+
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    protected function executeQuery(string $query): array
+    {
+        return Capsule::connection()->select($query);
     }
 }
